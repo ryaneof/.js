60da3a1fe1565826f8af9bf9b791c14796567ce2
ryaneof <ryaneof@gmail.com>
2014-08-03 16:30:48 +0800

    code style

--- zhihu.com.js
+++ zhihu.com.js
@@ -2,12 +2,13 @@
 
     var modifier = {
 
-    	expandAllAnswers: function () {
-    		// expand all answers if user is not logged in.
-    		$('.fixed-summary').removeClass('fixed-summary');
-    	},
+        expandAllAnswers: function () {
+            // expand all answers if user is not logged in.
+            $('.fixed-summary').removeClass('fixed-summary');
+        },
 
         unbindUselessEvents: function () {
+            // remove click event on answers if user is not logged in.
             $('.zm-editable-content').unbind('click');
         },
 
