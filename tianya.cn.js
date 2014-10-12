41f84b77ee5000a19b79468f3aa35aa9fd8a3896
ryaneof <ryaneof@gmail.com>
2014-10-13 00:26:36 +0800

    remove tianya.cn ads

---
+++ tianya.cn.js
@@ -0,0 +1,13 @@
+(function () {
+    $(document).on('ready', function () {
+        // remove left, right ad
+        $('#cproIframe2001holder').remove();
+        $('#BAIDU_DUP_fp_wrapper').remove();
+        $('#cproIframe2002holder').remove();
+
+        // bottom right corner ad
+
+        $('#ty_msg_mod').remove();
+        $('#travel_float').remove();
+    });
+})();
